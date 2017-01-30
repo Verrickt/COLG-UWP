625eaa9af73187cd6900892c5d91487b6d95e526
Von Hohenheim <adios_verrickt@hotmail.com>
2017-01-30 23:07:35 +0800

    Add CommandBar for all MenuPages

--- Colg UWP/View/Pages/MySpace.xaml.cs
+++ Colg UWP/View/Pages/MySpace.xaml.cs
@@ -24,7 +24,7 @@ namespace Colg_UWP.View.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MySpace : Page
+    public sealed partial class MySpace : MenuPage
     {
         public MySpace()
         {
