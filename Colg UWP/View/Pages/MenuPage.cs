d4a153732a01f5b72d5b99448381c0f3d80e80db
Von Hohenheim <adios_verrickt@hotmail.com>
2017-01-30 20:53:20 +0800

    Add a base class for all pages inside MenuFrame
    Avoid redundancy when getting ContentFrame

---
+++ Colg UWP/View/Pages/MenuPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Colg_UWP.View.Pages
+{
+    public class MenuPage:Page
+    {
+        protected Frame ContentFrame
+        {
+            get
+            {
+                var frame = (Frame)Window.Current.Content;
+                var page = (MainPage)frame.Content;
+                return page.ContentFrame;
+            }
+        }
+
+       
+    }
+}
