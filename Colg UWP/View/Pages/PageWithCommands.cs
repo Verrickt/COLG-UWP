3bb156926cc5fc5e83c1f7674a828ca29426fa92
Von Hohenheim <adios_verrickt@hotmail.com>
2017-01-31 22:31:26 +0800

    Add PageWithCommands
    PageWithCommands is the new basepage for the all pages in the app.
    It reperesent a page with a collection of commands which is used out of
    itself.

---
+++ Colg UWP/View/Pages/PageWithCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Colg_UWP.View.Pages
+{
+    public class PageWithCommands : Page
+    {
+        private MainPage _mainPage
+        {
+            get
+            {
+                var frame =
+                    (Frame) Window.Current.Content;
+                return
+                    (MainPage) frame.Content;
+            }
+        }
+
+        public virtual IObservableVector<ICommandBarElement> PrimaryCommands { get; private set; }
+
+        public virtual IObservableVector<ICommandBarElement> SecondaryCommands { get; private set; }
+
+        public virtual string Title { get; private set; }
+    }
+}
