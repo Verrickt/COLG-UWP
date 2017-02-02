66a5b3c6b9287967fb8b2553bd16ff691aca05e0
Von Hohenheim <adios_verrickt@hotmail.com>
2017-02-02 20:36:46 +0800

    Refactor

--- Colg UWP/View/Pages/PopularPostPage.xaml.cs
+++ Colg UWP/View/Pages/PopularPostPage.xaml.cs
@@ -1,15 +1,14 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Colg_UWP.Model;
 using Colg_UWP.ViewModel;
-using System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Colg_UWP.View.Pages
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class PopularPostPage : MenuPage
     {
@@ -23,40 +22,17 @@ namespace Colg_UWP.View.Pages
             InitializeComponent();
         }
 
-        private void PostList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            ContentFrame.Navigate(typeof(DiscussionPage),VM.SelectedPost);
-        }
-
-        private async void PopularPostView_OnLoading(FrameworkElement sender, object args)
-        {
-            if (VM == null)
-            {
-                VM = new PopularPostDiscussion();
-                await VM.RefreshAsync();
-            }
-        }
-
-        private async void Refresh_OnClick(object sender, RoutedEventArgs e)
-        {
+            VM = VM ?? new PopularPostDiscussion();
             await VM.RefreshAsync();
+            base.OnNavigatedTo(e);
         }
 
-        private void Top_OnClick(object sender, RoutedEventArgs e)
+        private void DiscussionList_OnItemClick(object sender, ItemClickEventArgs e)
         {
-            PostList.ScrollIntoView(PostList.Items[0]);
-        }
-
-        private void PostList_OnSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            //var gridview = sender as GridView;
-            //var wrapgrid = (ItemsWrapGrid)gridview.ItemsPanelRoot;
-            //int margin = 10;
-            //int n = (int)(e.NewSize.Width / (400 + margin));
-            //if (n != 0)
-            //{
-            //    wrapgrid.ItemWidth = e.NewSize.Width / n - margin;
-            //}
+            var discussion = e.ClickedItem as Discussion;
+            ContentFrame.Navigate(typeof(DiscussionPage), discussion);
         }
     }
 }
--- Colg UWP/ViewModel/PopularPostDiscussion.cs
+++ Colg UWP/ViewModel/PopularPostDiscussion.cs
@@ -3,24 +3,20 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Colg_UWP.Model;
 using Colg_UWP.Service;
+using Colg_UWP.Util;
 
 namespace Colg_UWP.ViewModel
 {
     public class PopularPostDiscussion:VMBase
     {
-        private ObservableCollection<Discussion> _popularPosts;
+        public RelayCommand RefreshCommand{ get; set; }
 
-        public ObservableCollection<Discussion> PopularPosts
-        {
-            get { return _popularPosts; }
-            set { SetProperty(ref _popularPosts, value); }
-        }
-
-        public Discussion SelectedPost { get; set; }
+        public ObservableCollection<Discussion> PopularPosts;
 
         public PopularPostDiscussion()
         {
-            _popularPosts= new ObservableCollection<Discussion>();
+            PopularPosts = new ObservableCollection<Discussion>();
+            RefreshCommand = new RelayCommand(async()=>await RefreshAsync(),()=>true);
         }
 
         public async Task RefreshAsync()
@@ -30,5 +26,6 @@ namespace Colg_UWP.ViewModel
             list.ForEach(post => PopularPosts?.Add(post));
         }
 
+       
     }
 }
