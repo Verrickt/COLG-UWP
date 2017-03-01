ae98ecea2d481ea76c6a8cbc08261a7878ebb836
Von Hohenheim <adios_verrickt@hotmail.com>
2017-03-01 15:06:21 +0800

    Fix a issue of SplitView.IsPaneOpen
    in overlay mode , pane is closed after item click.

--- Colg UWP/View/Pages/MainPage.xaml.cs
+++ Colg UWP/View/Pages/MainPage.xaml.cs
@@ -115,8 +115,11 @@ namespace Colg_UWP.View.Pages
         {
             MenuVM clicked = e.ClickedItem as MenuVM;
             bool
-                keepPaneOpen = MySplitView.DisplayMode == SplitViewDisplayMode.CompactInline;
-            MySplitView.IsPaneOpen = keepPaneOpen;
+                keepPaneClose = MySplitView.DisplayMode == SplitViewDisplayMode.Overlay;
+            if (keepPaneClose)
+            {
+                MySplitView.IsPaneOpen = false;
+            }
             if (clicked.TargetPage == null)
             {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
