7511edd837cd09b7a50c71562d04173cb0c5ac5e
Von Hohenheim <adios_verrickt@hotmail.com>
2017-03-17 12:51:43 +0800

    New converter

---
+++ Colg UWP/Resource/BooleanNotToVisibilityComverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Colg_UWP.Resource
+{
+    /// <summary>
+    /// True => Visibility.Collapsed
+    /// False => Visiblity.Visible
+    /// </summary>
+    class BooleanNegationToVisibilityConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
