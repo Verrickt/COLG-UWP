c81714d02b979f4888f64ab575e86af90fd2d436
Von Hohenheim <adios_verrickt@hotmail.com>
2017-03-18 21:09:27 +0800

    Exception Handeling

--- Colg UWP/ViewModel/ForumNaviVM.cs
+++ Colg UWP/ViewModel/ForumNaviVM.cs
@@ -7,6 +7,7 @@ namespace Colg_UWP.ViewModel
 {
     using Model;
     using Service;
+    using System;
     using System.Collections.ObjectModel;
 
     public class ForumNaviVM : VMBase
@@ -16,19 +17,42 @@ namespace Colg_UWP.ViewModel
 
         public Util.RelayCommand RefreshCommand { get; set; }
 
+        private bool _isLoading;
+
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set { SetProperty(ref _isLoading, value); }
+        }
+
+
         public ObservableCollection<ForumContainer> ForumContainers { get; set; }
 
         public async Task RefreshAsync()
         {
-            Forums = await ForumService.GetForumsAsync();
-            ForumContainers.Clear();
-            var containers = Forums.GroupBy(x => x.Catagory, (catagory, grouped) => new ForumContainer
+            try
             {
-                Catagory = catagory,
-                Forums
-             = new List<Forum>(grouped.OrderBy(f => f.Name[0]))
-            });
-            containers.ToList().ForEach(c => ForumContainers.Add(c));
+                IsLoading = true;
+                Forums = await ForumService.GetForumsAsync();
+                ForumContainers.Clear();
+                var containers = Forums.GroupBy(x => x.Catagory, (catagory, grouped) => new ForumContainer
+                {
+                    Catagory = catagory,
+                    Forums
+                 = new List<Forum>(grouped.OrderBy(f => f.Name[0]))
+                });
+                containers.ToList().ForEach(c => ForumContainers.Add(c));
+            }
+            catch (Exception e)
+            {
+                Logging.WriteLine($"Exception at ForumNaviVM.RefreshAsync{e}");
+                InAppNotifier.Show("网络不给力啊,要不刷新试试?");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+           
         }
 
         public ForumNaviVM()
