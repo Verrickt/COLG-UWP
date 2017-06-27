30095eef0f74c423626c6fe7f55688db5aeb8b55
Verrickt <Verrickt@users.noreply.github.com>
2017-06-27 14:17:28 +0800

    Refactor
    Merge converters

--- Colg UWP/Resource/Converters/BooleanToVisibilityConverter.cs
+++ Colg UWP/Resource/Converters/BooleanToVisibilityConverter.cs
@@ -4,24 +4,26 @@ using Windows.UI.Xaml.Data;
 
 namespace Colg_UWP.Resource.Converters
 {
+    /// <summary>
+    /// This converter maps true to Visiblity.Visible and false to Visiblity.Collapsed 
+    /// if IsInverted is set to false;
+    /// </summary>
     internal class BooleanToVisibilityConverter : IValueConverter
     {
+        public bool IsInverted { get; set; } = false;
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is bool)
+            if (value is bool b)
             {
-                return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+                return b^IsInverted ? Visibility.Visible : Visibility.Collapsed;
             }
             return Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            if (value is Visibility)
-            {
-                return (Visibility)value == Visibility.Visible ? true : false;
-            }
-            return Visibility.Collapsed;
+            throw new NotSupportedException();
         }
     }
 }
